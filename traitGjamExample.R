78da07918066b595c1bf1cedf7637f5926a7a2f1
rhinophile <bijan.text@gmail.com>
2016-03-31 16:28:04 -0400

    non

--- traitGjamExample.R
+++ traitGjamExample.R
@@ -1,54 +1,64 @@
 library(gjam)
 data(forestTraits)
-
 xdata       <- forestTraits$xdata
 traitTypes  <- forestTraits$traitTypes
-plotByTree  <- gjamReZero(forestTraits$treesDeZero)  # re-zero data
-
-tmp         <- gjamSpec2Trait(pbys = plotByTree, 
+plotByTrees <- gjamReZero(forestTraits$treesDeZero)  # re-zero data
+tmp         <- gjamSpec2Trait(pbys = plotByTrees, 
                               sbyt = forestTraits$specByTrait, 
                               tTypes = forestTraits$traitTypes)
-
-tTypes    <- tmp$traitTypes
-u         <- tmp$plotByCWM
-censor    <- tmp$censor
-specByCWM <- tmp$specByCWM
+tTypes      <- tmp$traitTypes
+u           <- tmp$plotByCWM
+censor      <- tmp$censor
+specByTrait <- tmp$specByTrait
 M           <- ncol(u)
 n           <- nrow(u)
 colnames(u)
 traitTypes
 
 
+
+censorList    <- gjamCensorY(values = c(0,1), intervals = cbind( c(-Inf,0),c(1,Inf) ),
+                             y = u, whichcol = c(13:14))$censor
+
+
 ###TRM
-tmp    <- gjamCensorY(values = c(0,1), intervals = cbind( c(-Inf,0),c(1,Inf) ),
-                      y = u, whichcol = c(13:14))
-censor <- append(censor, list('CA' = tmp$censor))
-
 modelList <- list(ng = 2000, burnin = 500, typeNames = tTypes,
-                  holdoutN = 20, xfactors='soil', censor=censor)
-
-output    <- gjamGibbs(~temp + stdage + deficit*soil, #~ temp + u1 + u2 + u3 + moisture*(deficit+soil),
+                  holdoutN = 20, censor=censor, notStandard = c('u1','u2','u3'))
+output    <- gjamGibbs(~ temp + u1 + u2 + u3 + moisture*(deficit+soil),
                        xdata = xdata, ydata = u, 
                        modelList = modelList)
+tnames    <- colnames(u)
+boxBorder <- rep('black', M)                          # highlight types
+boxCol    <- rep('grey',M)
+wo <- which(tnames %in% c("leafN","leafP","SLA") )    # foliar traits
+wf <- grep("leaf_",tnames)                            # leaf habit
+wc <- which(tnames %in% c("woodSG","xylem_ring") )    # wood anatomy
 
-plotPars  <- list(width = 3, height = 3, CLUSTERPLOTS = TRUE)           
+boxBorder[wc] <- 'brown'; boxCol[wc] <- 'tan'
+boxBorder[wf] <- 'darkblue'; boxCol[wf] <- 'lightblue'
+boxBorder[wo] <- 'darkgreen'; boxCol[wo] <- 'lightgreen'
+
+plotPars  <- list(width = 3, height = 3, GRIDPLOTS = TRUE,
+                  boxBorder = boxBorder, boxCol = boxCol)           
 fit       <- gjamPlot(output, plotPars)
 
 
+output$modelSummary$betaMu      # Q by M coefficient matrix alpha
+output$modelSummary$betaSe      # Q by M coefficient std errors
+output$modelSummary$sigMu       # M by M covariance matrix omega
+output$modelSummary$sigSe       # M by M covariance std errors
 
 
-##PRM
-
-specByTrait <- forestTraits$specByTrait
+fit$betaEstimates[1:5,]      # Q by M coefficient matrix alpha
 
 
-plotByTrees <- gjamReZero(forestTraits$treesDeZero)
-dim(plotByTrees)
+##PTM
 
 traitList <- list(plotByTrait = u, traitTypes = tTypes, 
-                  specByTrait = specByCWM)
+                  specByTrait = specByTrait)
 reductList <- list(r = 3, N = 20)
-modelList <- list(ng=1000,burnin=500, typeNames = 'CC', 
+
+modelList <- list(ng=2000, burnin=500, typeNames = 'CC', 
                   holdoutN = 20,
                   traitList = traitList, reductList = reductList)
 
@@ -56,11 +66,19 @@ output  <- gjamGibbs(~ temp + stdage + deficit*soil,
                      xdata = xdata, 
                      ydata = plotByTrees, modelList = modelList)
 
+S <- ncol(plotByTrees)
+boxBorder <- rep('black',S)
+boxCol <- rep('white',S)
+
+wr <- which(specByTrait[,'xylem_ring'] == 1)  # ring porous
+wb <- which(specByTrait[,'leaf_other'] == 1)  # broadleaf evergreen
+boxBorder[wr] <- 'brown'; boxCol[wr] <- 'tan'
+boxBorder[wb] <- 'darkgreen'; boxCol[wb] <- 'lightgreen'
+
 plotPars  <- list(width=4, height=4, corLines=F,
-                  SMALLPLOTS=F,CLUSTERPLOTS=T)                  
+                  SMALLPLOTS=F,GRIDPLOTS=T,
+                  boxBorder = boxBorder, boxCol = boxCol)                  
 fit       <- gjamPlot(output = output, plotPars)
-
-
 
 
 output$modelSummary$betaTraitMu   # Q by M coefficient matrix alpha
@@ -69,9 +87,5 @@ output$modelSummary$sigmaTraitMu  # M by M covariance matrix omega
 output$modelSummary$sigmaTraitSe  # M by M covariance std errors
 
 
-
-
 output$modelSummary$tMu[1:5,]     # n by M predictive means
 output$modelSummary$tSd[1:5,]   # n by M predictive std errors
-
-
