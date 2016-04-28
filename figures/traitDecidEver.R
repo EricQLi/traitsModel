6b5c0c381df5e96d02434037b96fee52b80d021d
rhinophile <bijan.text@gmail.com>
2016-04-28 13:49:28 -0400

    NP ratio in

---
+++ figures/traitDecidEver.R
@@ -0,0 +1,40 @@
+
+
+png('figures/Decid-Ever.png', units='in',res=300, height  = 10, width=15)
+par(mfrow=c(2,3),oma = c(3,7,5,5),mar = c(0,0,0,0))
+
+for(i in 1:2){
+  for(j in 7:9){
+    
+    ssj.obs <- traitMuAll[,j]     #observed traits
+    ssj.pred <- tMu[,j] #predicted traits
+    
+    ssj <- switch(i, ssj.obs, ssj.pred)
+    ssj <-  (ssj>switch(j-6, 0.9999, .01, .1))*1
+    ssj[ssj==0] <- NA
+    par(xaxt='n', yaxt='n')
+    mapColorData(lonLatAll[,1],lonLatAll[,2], ssj, xlim = range(lonLatAll[,1]), ylim = range(lonLatAll[,2]),
+                 valRange = c(0,1),colList = c('white','#1A9850A0'), ADD=F, cex.all = 2, legendShow = F )
+    mapOutlines(lwd2 = 8)
+    
+    if(i==1)mtext(text =  switch(j-6, 'Deciduous','BL Evergreen','NL Evergreen'), side = 3, line = 2, cex=2)
+    if(i==1)mtext(text =  switch(j-6, '(a)','(b)','(c)'), side = 1, line = -2,at = -68, cex=2)
+    if(i==2)mtext(text =  switch(j-6, '(d)','(e)','(f)'), side = 1, line = -2,at = -68, cex=2)
+    
+    if(i==2)
+    {
+      par(xaxt='s', yaxt='s')
+      # axis(1, cex.axis=1.7)
+      par(xaxt='n', yaxt='n')
+    }
+    if(j==7)
+    {
+      par(xaxt='s', yaxt='s')
+      # axis(2, cex.axis=1.7)
+      par(xaxt='n', yaxt='n')
+      mtext(text = switch(i, 'Observed', 'Predicted'), side = 2, cex=2, line = 3)
+    }
+    
+  }
+}
+dev.off()
--- figures/traitNPratio.R
+++ figures/traitNPratio.R
@@ -1,5 +1,15 @@
+source('~/Projects/procVisData/dataViz.R')
+
+source('~/Projects/procVisData/geoSpatial.R')
+source('~/Projects/procVisData/colorProc.R')
+
+source('traitPreModel.R')
+source('traitFunctions.R')
+source('figures/traitColorSet.R')
+
 #####################################
 plotNP <- CWT$perMass[,'N']/CWT$perMass[,'P']
+g <- data.frame(lat=plotByX$plotLat, lon =plotByX$plotLon, NP=plotNP, elev =plotByX$elev)
 
 png('figures/NP-ratio.png', units='in',res=300, height  = 8, width=8)
 layout(matrix(c(3,1,1,3,1,1,4,2,2),3,3))
@@ -10,14 +20,13 @@ par(mar=c(0,0,0,0), oma=c(5,5,5,5), cex.axis=1.7)
 mapColorData(valRange = quantile(plotNP, probs=c(.1,.9)), 
              x = plotByX$plotLon, y = plotByX$plotLat, data = plotNP,
              colList = colList.purpleOrange, cex.all = 2)
-mapOutlines(lwd2 = 10)
+mapOutlines(glacialLine, ecoRegion, lwd2 = 10)
 
 mtext(text = 'Leaf N:P', side = 3, line = -3, cex=1.5)
 mtext(text = 'Longitude (°)', side = 1, line = 3, cex=1.5)
 mtext(text = 'Latitude (°)', side = 2, line = 3, cex=1.5)
 mtext('(a)', 3, at = -68, line = -2.5, cex=2)
 
-g <- gridData1
 par(xaxt='s', yaxt='n',bty='n')
 tmp = myBinPlot(g$lat, g$NP, xbreaks = seq(25,50, 3), noPlot = T)
 boxplot(tmp$bins, horizontal = T, varwidth = T, outline = F)
@@ -39,7 +48,7 @@ par(xaxt='s',yaxt='s')
 axis(3, at = 1:8, labels = c(1:8)*100)
 lines(tmp$q[,3], lwd=10, col='#80808080')
 axis(4)
-mtext(text = 'Elevation (m)', side = 3, line = 3, cex=1.5)
+mtext(text = 'Altitude (m)', side = 3, line = 3, cex=1.5)
 mtext(text = expression(bar('N:P')), side = 4, line = 3, cex=1.5)
 mtext('(d)', 3, cex=2 , at= 10, line=2)
 
