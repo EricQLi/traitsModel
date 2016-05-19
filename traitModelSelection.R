7ba3259460dc478d3b5031f9a23a626735dcfbb7
rhinophile <bijan.text@gmail.com>
2016-05-18 21:13:16 -0400

    model #6: temp + moisture + deficit + therm  + soil

--- traitModelSelection.R
+++ traitModelSelection.R
@@ -23,8 +23,8 @@ modelList <- list(ng=2000,
 attr(plotByX$soil,'reference') <- 'Others'   # reference class
 attr(plotByX$soil,'intType')   <- 'ref'
 
-modelNo <- 5
-output  <- gjamGibbs(~ temp + moisture + therm  + soil 
+modelNo <- 6
+output  <- gjamGibbs(~ temp + moisture + deficit + therm  + soil
                      #temp  +  deficit + moisture + therm + soil 
                      #u1 + u2 + u3 + 
                      #u1*temp + u2*temp + u3*temp
