2fe65a9dfb9ba11768f7df3694bd076b64333d94
rhinophile <bijan.text@gmail.com>
2016-05-19 07:48:27 -0400

    automated model selection and model #10: temp + deficit + therm + soil +u1 + u2 + u3

--- traitModelSelection.R
+++ traitModelSelection.R
@@ -23,33 +23,30 @@ modelList <- list(ng=2000,
 attr(plotByX$soil,'reference') <- 'Others'   # reference class
 attr(plotByX$soil,'intType')   <- 'ref'
 
-modelNo <- 9
-output  <- gjamGibbs(~ temp + moisture + therm + soil +u1 + u2 + u3 
-                     #temp  +  deficit + moisture + therm + soil 
-                     #u1 + u2 + u3 + 
-                     #u1*temp + u2*temp + u3*temp
-                     #deficit*therm +
-                     #deficit*temp + therm*temp +		      
-                     #deficit*moisture + temp*moisture + therm*moisture +		      
-                     #moisture*soil + deficit*soil + temp*soil + therm*soil
-                     ,   
-                     xdata = plotByX, 
-                     ydata = plotByW, 
-                     modelList = modelList)
+models <- readLines('traitModel.Models.txt')
 
-save.image(paste('modelSelection/output', modelList$ng/1000, modelList$burnin/1000,
-                 substring(make.names(Sys.time()),2), '.RData', sep = '-'))
-
-modelSelectSumm.Cur <- data.frame(modelNo=modelNo,
-                                  time =as.character(Sys.time()),
-                                  DIC=output$modelSummary$DIC,
-                                  yScore=mean(output$modelSummary$yscore),
-                                  xScore=mean(output$modelSummary$xscore, na.rm = T))
-
-modelSelectSumm <- read.csv('modelSelection/modelSelectSumm.csv')
-
-modelSelectSumm <- rbind(modelSelectSumm, modelSelectSumm.Cur)
-
-write.csv(modelSelectSumm, file = 'modelSelection/modelSelectSumm.csv', row.names = F)
-
+for(modelNo in 11:length(models)){
+  set.seed(2016)
+  
+  output  <- gjamGibbs(as.formula(models[modelNo]),
+                       xdata = plotByX, ydata = plotByW, 
+                       modelList = modelList)  
+  
+  strTime <- substring(make.names(Sys.time()),2)
+  
+  save.image(paste('modelSelection/output', modelNo,
+                   modelList$ng/1000, modelList$burnin/1000,
+                   strTime, '.RData', sep = '-'))
+  
+  modelSelectSumm.Cur <- data.frame(modelNo=modelNo,
+                                    time =strTime,
+                                    DIC=output$modelSummary$DIC,
+                                    yScore=mean(output$modelSummary$yscore),
+                                    xScore=mean(output$modelSummary$xscore, na.rm = T))
+  
+  modelSelectSumm <- read.csv('modelSelection/modelSelectSumm.csv')
+  modelSelectSumm <- rbind(modelSelectSumm, modelSelectSumm.Cur)
+  write.csv(modelSelectSumm, file = 'modelSelection/modelSelectSumm.csv', row.names = F)
+  
+}
 
