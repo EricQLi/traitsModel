13f8be275fea9410a381f8e6aefa353b31c2fa07
rhinophile <bijan.text@gmail.com>
2016-05-31 09:19:28 -0400

    top 15 models with ng= 2000 in

--- modelSelection/traitModelComapreDICConvergence.R
+++ modelSelection/traitModelComapreDICConvergence.R
@@ -6,11 +6,18 @@ colnames(mod55)[4] <- 'DIC.55'
 colnames(mod300)[4] <- 'DIC.300'
 colnames(mod15)[4] <- 'DIC.15'
 
-mod <- merge(mod55, mod300, by='Model.No')
+mod <- merge(mod300, mod55, by='Model.No')
+plot(mod[,c( 'DIC.55','DIC.300')])
 
-mod <- merge(mod55, mod15, by='Model.No')
 
-plot(mod[,c('DIC.15', 'DIC.55','DIC.300')])
+mod <- merge(mod, mod15, by='Model.No')
+plot(mod[,c('DIC.15', 'DIC.55','DIC.300')], col=mod$Model.No)
+plot(mod[,c('xScore.x', 'xScore.y','xScore')], col=mod$Model.No)
+plot(mod[,c('yScore.x', 'yScore.y','yScore')], col=mod$Model.No)
 
-plot(mod$xScore.x, mod$xScore.y)
-plot(mod$yScore.x, mod$yScore.y)
+abline(0,1)
+par(mfrow=c(1,3))
+plot(mod[,c( 'DIC.55','DIC.15')], col=mod$Model.No);abline(0,1)
+plot(mod[,c( 'xScore.y','xScore')], col=mod$Model.No);abline(0,1)
+plot(mod[,c( 'yScore.y','yScore')], col=mod$Model.No);abline(0,1)
+#plot(mod[,c('DIC.15','xScore','yScore')])
