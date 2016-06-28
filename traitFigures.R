8a3b4f7f7407d116c9b1d65167d64290db4c6173
rhinophile <bijan.text@gmail.com>
2016-06-28 13:00:38 -0400

    new run ... no surplus interactions: soil with all + def w/ mois

--- traitFigures.R
+++ traitFigures.R
@@ -4,13 +4,20 @@ source('figures/traitFig.N-P-SLA.Mass.R')
 source('figures/traitFig.N-P.Area.R')
 source('figures/traitFig.N-P.Ratio.R')
 
+# source('figures/traitFig.Soil.CN.Zinke.R')
+# source('figures/traitFig.SoilCNP.Xu.R')
+# source('figures/traitFig.Soil.P.Yang.R')
+source('figures/traitFig.Soil.P.Yang.Sub.R')
+source('figures/traitFig.Soil.MicrobBiomass.Xu.R')
+source('figures/traitFig.MapSpecies.High.Low.NPSLA.R')
+
+
 source('figures/traitFig.Decid-Ever.R')
 source('figures/traitFig.CondOnLeaf.R')
 #source('figures/traitFig.FilterOnLeaf.R')
 
 source('figures/traitFig.PostProc.Climate.R')
 source('figures/traitFig.PostProc.Soil.R')
-
 source('figures/traitFig.PostProc.Interactions.Moisture.R')
 source('figures/traitFig.PostProc.Interactions.Deficit.R')
 source('figures/traitFig.PostProc.Interactions.Temperature.R')
@@ -20,12 +27,3 @@ source('figures/traitFig.SensDeficit.R')
 source('figures/traitFig.SensTemp.R')
 
 source('figures/traitFig.AspectEffect.R')
-
-# source('figures/traitFig.Soil.CN.Zinke.R')
-# source('figures/traitFig.SoilCNP.Xu.R')
-# source('figures/traitFig.Soil.P.Yang.R')
-source('figures/traitFig.Soil.P.Yang.Sub.R')
-source('figures/traitFig.Soil.MicrobBiomass.Xu.R')
-
-source('figures/traitFig.MapSpecies.High.Low.NPSLA.R')
-
