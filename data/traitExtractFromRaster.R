426abcddc1003acc91f25a47a3e8428656199d7f
rhinophile <bijan.text@gmail.com>
2016-07-20 12:23:06 -0400

    exracted soil grids data in

--- data/traitExtractFromRaster.R
+++ data/traitExtractFromRaster.R
@@ -1,14 +1,48 @@
 library(raster)
+library(data.table)
 
-r <- raster('~/Google Drive/MODIS/TAXNWRB_250m_ll.tif')
+
+rasterFolder <- '~/../Downloads/SoilGrids/'
+
+taxUSDA <- raster(paste0(rasterFolder,'TAXOUSDA_250m_ll.tif'))
+taxNWRB <- raster(paste0(rasterFolder,'TAXNWRB_250m_ll.tif'))
+sand <- raster(paste0(rasterFolder,'SNDPPT_M_sl1_250m_ll.tif'))
+silt <- raster(paste0(rasterFolder,'SLTPPT_M_sl1_250m_ll.tif'))
+clay <- raster(paste0(rasterFolder,'CLYPPT_M_sl1_250m_ll.tif'))
+ph <- raster(paste0(rasterFolder,'PHIHOX_M_sl1_250m_ll.tif'))
+orgCct <- raster(paste0(rasterFolder,'ORCDRC_M_sl1_250m_ll.tif'))
+orgCst<- raster(paste0(rasterFolder,'OCSTHA_M_sd1_250m_ll.tif'))
+
 spCoords <- read.csv('data/pre/lonLatAll.csv')
 ww <- read.csv('data/pre/wwAll.csv')$w
-
 so <- read.csv('data/pre/soilAll.csv', header = F)
 
-rEx <- extract(r, spCoords)
- 
-library(data.table)
-tmp <- as.data.table(cbind(so, rEx))
+exTaxUSDA <- extract(taxUSDA, spCoords)
+exSand <- extract(sand, spCoords)
+exSilt <- extract(silt, spCoords)
+exClay <- extract(clay, spCoords)
+exPh <- extract(ph, spCoords)
+exOrgCct <- extract(orgCct, spCoords)
+exOrgCst <- extract(orgCst, spCoords)
 
-tmp[,.N, .(rEx, V1)]
+soilGrids <- data.table(tax= exTaxUSDA, 
+           sand= exSand,
+           silt=exSilt,
+           clay=exClay,
+           ph= exPh,
+           orgCst=exOrgCst,
+           orgCct= exOrgCct)
+
+rownames(soilGrids) <- rownames(spCoords)
+
+
+taxCode <- read.csv('data/post/soilTaxUSDA.csv')
+taxCode <- as.data.table(taxCode)
+
+soilGrids[,ID:= tax]
+
+soilGrids <- merge(soilGrids, taxCode, by='ID')
+soilGrids$ID <- NULL
+soilGrids$Color <- NULL
+
+write.csv(soilGrids, 'data/post/soilGrids.ext.csv')
